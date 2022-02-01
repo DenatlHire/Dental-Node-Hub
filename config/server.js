7297ad311607a4936b5b5df120563ac6d7b1e924
Snehal <patelsnehal2101992@gmail.com>
2022-02-01 22:15:02 +0530

    New Changes

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('', 'http://localhost:1337'),
-  //url: env('', 'http://138.197.160.154:1337'),
+   url: env('', 'http://localhost:1337'),
+//  url: env('', 'http://dentalhire.ca:1337'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd061a4cfadb914ab2eede9e6c152eceb'),
